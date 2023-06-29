9ff5fdfd13b44d469940864e21149a27bdbc00d7
DimonTheGood <almakaevdima7@mail.ru>
2023-06-30 01:58:13 +0500

    first commit

---
+++ xd123.cpp
@@ -0,0 +1,16 @@
+﻿#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+
+
+int main() {
+	int t; cin >> t;
+	vector<int> res; 
+	string a; cin >> a;
+	for (int i = 0; i < t; i++) {
+		res.push_back(a[i]);
+	}
+	erase(res, 1);
+
+}
